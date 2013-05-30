37152282e4a98559f16d6f2c54ea003f6c265e21
John Bresnahan <jbresnah@redhat.com>
2013-05-30 11:04:41 -1000

    serializer tests

--- staccato/tests/unit/v1/test_api.py
+++ staccato/tests/unit/v1/test_api.py
@@ -1,8 +1,11 @@
 import json
 import testtools
+import uuid
 
 from staccato.tests import utils
 import staccato.api.v1.xfer as v1_xfer
+import staccato.db.models as db_models
+import staccato.xfer.constants as constants
 
 import webob.exc
 
@@ -75,3 +78,53 @@ class TestDeserializer(utils.TempFileCleanupBaseTest):
         self.assertRaises(webob.exc.HTTPBadRequest,
                           xd.xferaction,
                           body)
+
+
+class TestSerializer(utils.TempFileCleanupBaseTest):
+
+    def setUp(self):
+        super(TestSerializer, self).setUp()
+        self.serializer = v1_xfer.XferSerializer()
+
+    def _check_xfer(self, xfer, d):
+        self.assertEqual(xfer.id, d['id'])
+        self.assertEqual(xfer.srcurl, d['source_url'])
+        self.assertEqual(xfer.dsturl, d['destination_url'])
+        self.assertEqual(xfer.state, d['state'])
+        self.assertEqual(xfer.start_ndx, d['start_offset'])
+        self.assertEqual(xfer.end_ndx, d['end_offset'])
+        self.assertEqual(xfer.source_opts, d['source_options'])
+        self.assertEqual(xfer.dest_opts, d['destination_options'])
+
+    def _make_xfer_request(self):
+        x = db_models.XferRequest()
+        x.id = str(uuid.uuid4())
+        x.state = constants.States.STATE_RUNNING
+        x.srcurl = "http://%s.com/path" % str(uuid.uuid4())
+        x.dsturl = "file://%s.com/path" % str(uuid.uuid4())
+        x.start_ndx = 0
+        x.end_ndx = 150
+        x.source_opts = {'somevalue': str(uuid.uuid4())}
+        x.dest_opts = {'destvalue': str(uuid.uuid4())}
+        return x
+
+    def test_default(self):
+        x = self._make_xfer_request()
+        ds = self.serializer.default(x)
+        d = json.loads(ds)
+        self._check_xfer(x, d)
+
+    def test_list(self):
+        x1 = self._make_xfer_request()
+        x2 = self._make_xfer_request()
+        x3 = self._make_xfer_request()
+        x4 = self._make_xfer_request()
+        xfer_list = [x1, x2, x3, x4]
+        lookup = {}
+        for x in xfer_list:
+            lookup[x.id] = x
+        result_str = self.serializer.list(xfer_list)
+        result_list = json.loads(result_str)
+        for x_d in result_list:
+            x = lookup[x_d['id']]
+            self._check_xfer(x, x_d)
